2ea37752cec1b0adb5d461e98657c17e2f99c3c1
Vladislav <serov.vladsilaw@yandex.ru>
2022-12-06 19:32:43 +0300

    feat: add controller for delete file or directory

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -63,26 +63,31 @@ class FileController {
 
             user.usedSpace = parseInt(user.usedSpace) + parseInt(file.size);
 
-            let pathFile;
+            let pathFileSystem;
             if (parent) {
-                pathFile = path.join(__dirname, '..', 'files', `${userId}`, `${parent.path}`, `${file.name}`);
+                pathFileSystem = path.join(__dirname, '..', 'files', `${userId}`, `${parent.path}`, `${file.name}`);
             } else {
-                pathFile = path.join(__dirname, '..', 'files', `${userId}`, `${file.name}`);
+                pathFileSystem = path.join(__dirname, '..', 'files', `${userId}`, `${file.name}`);
             }
 
 
-            if (fs.existsSync(pathFile)) {
+            if (fs.existsSync(pathFileSystem)) {
                 next(ApiError.internal('Error', 'File already exist'))
             }
 
-            await file.mv(pathFile);
+            await file.mv(pathFileSystem);
 
             const type = path.extname(file.name).slice(1);
+            let pathFile = file.name;
+            if (parent) {
+                pathFile = path.join(parent.path, file.name);
+            }
+
             const dbFile = await File.create({
                 name: file.name,
                 type,
                 size: file.size,
-                path: parent?.path,
+                path: pathFile,
                 parentId: parent?.id,
                 userId
             })
@@ -111,6 +116,26 @@ class FileController {
         } catch(e) {
             console.log(e);
             next(ApiError.internal('Download error', e));
+        }
+    }
+
+    async deleteFile(req, res, next) {
+        try {
+            let file = await sequelize.query(`SELECT * FROM "files" AS "file" WHERE "file"."id" = ${req.query.id} AND "file"."userId" = ${req.user.id}`, { type: QueryTypes.SELECT });
+            file = file[0];
+
+            if (!file) {
+                next(ApiError.badRequest('file not found'))
+            }
+            console.log(1)
+            fileService.deleteFile(file);
+            console.log(2)
+
+            await sequelize.query(`DELETE FROM "files" WHERE "files"."id" = ${req.query.id} AND "files"."userId" = ${req.user.id}`, {type: QueryTypes.DELETE});
+            return res.json({message: 'File was deleted'})
+        } catch (e) {
+            console.log(e);
+            next(ApiError.badRequest('Dir is not empty'))
         }
     }
 }
--- services/fileService.js
+++ services/fileService.js
@@ -19,6 +19,18 @@ class FileService {
         })
     }
 
+    deleteFile(file) {
+        const path = this.getPath(file);
+        if (file.type === 'dir') {
+            fs.rmdirSync(path);
+        } else {
+            fs.unlinkSync(path);
+        }
+    }
+
+    getPath(file) {
+        return path.join(__dirname, '..', 'files', file.userId.toString(), file.path);
+    }
 }
 
 module.exports = new FileService();
